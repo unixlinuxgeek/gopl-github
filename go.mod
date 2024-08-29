6194bbc129af59a1f871c5d1b01332cf5f39df4c
unixlinuxgeek <unixlinuxgeek@gmail.com>
2024-08-30 01:57:25 +0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/unixlinuxgeek/gopl-github
+
+go 1.22.4
---
+++ gopl-github.go
@@ -0,0 +1,56 @@
+// Пакет github предоставляет Go API для хостинга Github.
+// См.https://developer.github.com/v3/search/$searchissues.
+
+package github
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+const IssuesURL = "https://api.github.com/search/issues"
+
+type IssuesSearchResult struct {
+	TotalCount int `json:"total_count"`
+	Items      []*Issue
+}
+type Issue struct {
+	Number    int
+	HTMLURL   string `json:"html_url"`
+	Title     string
+	State     string
+	User      *User
+	CreatedAt time.Time `json:"created_at"`
+	Body      string
+}
+
+type User struct {
+	Login   string
+	HTMLURL string `json:"html_url"`
+}
+
+// SearchIssues запрашивает Github.
+func SearchIssues(terms []string) (*IssuesSearchResult, error) {
+	q := url.QueryEscape(strings.Join(terms, " "))
+	resp, err := http.Get(IssuesURL + "?q=" + q)
+	if err != nil {
+		return nil, err
+	}
+	// Необходимо закрыть resp.Body на всех путях выполнения.
+	// (В главе 5 вы познакомитесь с более простым решением: `defer`.)
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("Сбой запроса: %s", resp.Status)
+	}
+	var result IssuesSearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
+}
